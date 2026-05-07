f1099484e13fdf7a955b94a5df567c0da10f82e1
ankun-eric <ankun-eric@users.noreply.github.com>
2026-05-07 18:19:59 +0800

    feat: 页面左上角标题改为'使劲吹牛逼的系统'

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -85,12 +85,12 @@ export default function Dashboard() {
   return (
     <>
       <Head>
-        <title>超级大牛逼的任务管理系统 - 仪表盘</title>
+        <title>使劲吹牛逼的系统 - 仪表盘</title>
       </Head>
 
       <header className="header">
         <div className="container header-content">
-          <span className="logo">超级大牛逼的任务管理系统</span>
+          <span className="logo">使劲吹牛逼的系统</span>
           <Link href="/tasks/create" className="btn btn-primary">
             + 创建任务
           </Link>
